57c48726feea391560106468202437ced1851fd4
Kyle Pasag <kylepasag@gmail.com>
2025-11-02 21:26:56 -0800

    Add quote of the day for main function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import { db } from "./firebaseConfig.js"
+import { doc, onSnapshot } from "firebase/firestore"
 import { onAuthReady } from "./authentication"
 
 function showDashboard() {
@@ -8,13 +10,32 @@ function showDashboard() {
       location.href = "index.html"
       return
     }
-
     const name = user.displayName || user.email
-
     if (nameElement) {
       nameElement.textContent = `${name}!`
     }
   })
 }
 
+function readQuote(day) {
+  const quoteDocRef = doc(db, "quotes", day) // Get a reference to the document
+
+  onSnapshot(
+    quoteDocRef,
+    (docSnap) => {
+      // Listen for real-time updates
+      if (docSnap.exists()) {
+        document.getElementById("quote-goes-here").innerHTML =
+          docSnap.data().quote
+      } else {
+        console.log("No such document!")
+      }
+    },
+    (error) => {
+      console.error("Error listening to document: ", error)
+    }
+  )
+}
+
+readQuote("monday")
 showDashboard()
